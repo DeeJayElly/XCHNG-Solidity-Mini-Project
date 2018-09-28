99cfe27a47e7df920d2ceb5103b0659e0f0449d5
Aleksandar Djordjevic <adjordjevic@firstbeatmedia.com>
2018-09-28 14:57:16 +0200

    Adding documentation for each method

--- contracts/CarExchange.sol
+++ contracts/CarExchange.sol
@@ -13,6 +13,7 @@ contract CarExchange is Ownable {
   // We emit this event whenever a car is listed for sale
   event Listed(uint indexed _vinNumber, address indexed _carOwner, uint _value);
 
+  // Car structure
   struct Car {
     uint value;
     address owner;
@@ -22,6 +23,8 @@ contract CarExchange is Ownable {
 
   // A list of all registered cars
   uint[] cars;
+
+  // Mapping which contains all car id'' connected to their car structures
   mapping(uint => Car) public carStructs;
 
   // A list of registered cars per owner
@@ -30,9 +33,19 @@ contract CarExchange is Ownable {
   // A list of car count per owner
   mapping(address => uint256) ownerCarCount;
 
+  /**
+   * @dev CarExchange constructor function.
+   */
   constructor() public {
   }
 
+  /**
+   * @dev Find index of owned cars function.
+   *
+   * @param _owner address - The address of owner of the car.
+   * @param _vinNumber uint - Vin number of the car (id of the car).
+   * @return uint - Index of the car.
+   */
   function indexOf(address _owner, uint _vinNumber) internal view returns (uint) {
     uint i = 0;
     while (ownerToCars[_owner][i] != _vinNumber) {
@@ -41,6 +54,13 @@ contract CarExchange is Ownable {
     return i;
   }
 
+  /**
+   * @dev Remove ownership at the give index function.
+   *
+   * @param _owner address - The address of owner of the car.
+   * @param _index uint - Index of the given car.
+   * @return bool - Success if the removal of the ownership has happen.
+   */
   function removeOwnershipAtIndex(address _owner, uint _index) internal returns (bool success) {
     if (_index >= ownerToCars[_owner].length) return;
 
@@ -53,12 +73,26 @@ contract CarExchange is Ownable {
     return true;
   }
 
+  /**
+   * @dev Remove ownership function.
+   *
+   * @param _owner address - The address of owner of the car.
+   * @param _vinNumber uint - Vin Number (index) of the given car.
+   * @return bool - Success if the removal of the ownership has happen.
+   */
   function removeOwnership(address _owner, uint _vinNumber) public returns (bool success) {
     uint index = indexOf(_owner, _vinNumber);
     removeOwnershipAtIndex(_owner, index);
     return true;
   }
 
+  /**
+   * @dev Register car function.
+   *
+   * @param _owner address - The address of owner of the car.
+   * @param _vinNumber uint - Vin Number (index) of the given car.
+   * @return bool - Success if the removal of the ownership has happen.
+   */
   function register(address _owner, uint _vinNumber) public returns (bool success) {
     require(_owner != address(0));
 
@@ -76,6 +110,13 @@ contract CarExchange is Ownable {
     return true;
   }
 
+  /**
+   * @dev Buy car function.
+   *
+   * @param _vinNumber uint - Vin number (index) of the car.
+   * @param _value uint - Price of the car.
+   * @return bool - Success if the removal of the ownership has happen.
+   */
   function buy(uint _vinNumber, uint _value) public payable returns (bool success) {
     // require that the buyer is a true person
     require(msg.sender != address(0));
@@ -104,6 +145,13 @@ contract CarExchange is Ownable {
     return true;
   }
 
+  /**
+   * @dev List car for sale function.
+   *
+   * @param _vinNumber uint - Vin number (index) of the car.
+   * @param _value uint - Price of the car.
+   * @return bool - Success if the removal of the ownership has happen.
+   */
   function list(uint _vinNumber, uint _value) public returns (bool success) {
     //require that the user is a true user
     require(msg.sender != address(0));
@@ -119,6 +167,12 @@ contract CarExchange is Ownable {
     return true;
   }
 
+  /**
+   * @dev Return owned cars function.
+   *
+   * @param _owner address - The address of owner of the cars.
+   * @return vinNumbers uint[] - Vin numbers (indexes) of the cars owned by the given owner.
+   */
   function ownedCars(address _owner) external view returns (uint[] vinNumbers) {
     require(_owner != address(0));
     return ownerToCars[_owner];
